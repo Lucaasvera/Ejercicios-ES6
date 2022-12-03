4c6cafd191c7df578b97a91e69ff2a277633b872
Lucas <lucaasveraa99@gmail.com>
2022-12-02 23:12:01 -0300

    Subiendo desde gitbash

---
+++ es6.js
@@ -0,0 +1,42 @@
+//Ejercicio 1 Compara el alcance de var y let
+function checkScope() {
+  let i = "function scope";
+  if (true) {
+    let i = "block scope";
+    console.log("Block scope i is: ", i);
+  }
+  console.log("Function scope i is: ", i);
+  return i;
+}
+
+//Ejercicio 2 Muta un arreglo declarado con const
+
+const s = [5, 7, 2];
+function editInPlace() {
+  s[0] = 2;
+  s[1] = 5;
+  s[2] = 7;
+}
+editInPlace();
+
+//Ejercicio 3 Prevenir la mutación del objeto
+
+function freezeObj() {
+  const MATH_CONSTANTS = {
+    PI: 3.14,
+  };
+
+  Object.freeze(MATH_CONSTANTS);
+
+  try {
+    MATH_CONSTANTS.PI = 99;
+  } catch (ex) {
+    console.log(ex);
+  }
+  return MATH_CONSTANTS.PI;
+}
+const PI = freezeObj();
+
+//Ejercicio 4 Usa funciones flecha para escribir funciones anónimas de manera breve
+
+let magic = () => new Date();
